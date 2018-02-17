b671482e3e7b12cc30d3e01ec06c061a5eba15d4
Marc Hoersken <info@marc-hoersken.de>
2018-02-17 15:38:13 +0100

    Overwrite links if they do already exist

--- docker-entrypoint.d/03-create-links.sh
+++ docker-entrypoint.d/03-create-links.sh
@@ -2,11 +2,11 @@
 set -e
 
 # link to data directories
-ln -s /data/logs  /app/teamspeak3/logs
-ln -s /data/files /app/teamspeak3/files
+ln -sf /data/logs  /app/teamspeak3/logs
+ln -sf /data/files /app/teamspeak3/files
 
 # link to license files
-ln -s /data/licensekey.dat /app/teamspeak3/licensekey.dat
-ln -s /data/serverkey.dat  /app/teamspeak3/serverkey.dat
+ln -sf /data/licensekey.dat /app/teamspeak3/licensekey.dat
+ln -sf /data/serverkey.dat  /app/teamspeak3/serverkey.dat
 
 exit 0
